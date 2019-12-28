32728565effe1fcf0b22ad6a1f15e2ed3c889473
Suborna Jahan <suboorna@gmail.com>
2019-12-28 16:02:28 +0100

    watched tutorial 6 and 7

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -78,3 +78,140 @@ var templateTwo = React.createElement(
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(templateTwo, appRoot);
+
+//Tutorial: 6. JSX Expressions
+
+var user = {
+    name: 'iraamoni',
+    age: 27,
+    location: 'London, United Kingdom' // using object 
+
+};var templateThree = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
+var userName = 'Iraamoni';
+var userAge = 27;
+var userLocation = 'London, United Kingdom';
+// using direct variables 
+
+var templateThree = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        userName
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        userAge
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        userLocation
+    )
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateThree, appRoot);
+
+//Challenge: 6. JSX Expressions
+//create app object: string/subtitle
+// use title/subtitle
+//render template
+
+var userInfo = {
+    title: 'Suborna J',
+    subtitle: 'This is a short info about Suborna'
+};
+
+var templateFour = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h2',
+        null,
+        'Title: ',
+        userInfo.title
+    ),
+    React.createElement(
+        'h3',
+        null,
+        'Subtitle: ',
+        userInfo.subtitle
+    )
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateFour, appRoot);
+
+//Tutorial: 7. Conditional Rendering in JSX
+
+var userInfo = {
+    title: 'Suborna Jahan',
+    subtitle: 'This is a short info about Suborna',
+    job: 'UI/UX designer',
+    age: 27
+};
+
+function getSubtitle(subtitle) {
+    if (subtitle) {
+        return React.createElement(
+            'h3',
+            null,
+            'Subtitle: ',
+            subtitle
+        );
+    }
+}
+
+var templateFive = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        userInfo.title ? userInfo.title : 'Anonymous'
+    ),
+    getSubtitle(userInfo.subtitle),
+    React.createElement(
+        'h4',
+        null,
+        'Job: ',
+        userInfo.job
+    ),
+    userInfo.age && userInfo.age >= 18 && React.createElement(
+        'h5',
+        null,
+        'Age: ',
+        userInfo.age
+    )
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateFive, appRoot);
+
+//Challenge: 7. Conditional Rendering in JSX
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -39,6 +39,86 @@
 
     ReactDOM.render(templateTwo, appRoot)
 
+    //Tutorial: 6. JSX Expressions
+
+    var user = {
+        name: 'iraamoni',
+        age: 27,
+        location: 'London, United Kingdom'
+    } // using object 
+
+    var templateThree = (
+        <div>
+            <h1>{user.name}</h1>
+            <p>Age: {user.age}</p>
+            <p>Location: {user.location}</p>
+        </div>
+    );
+
+    var userName = 'Iraamoni';
+    var userAge = 27; 
+    var userLocation = 'London, United Kingdom'
+    // using direct variables 
+
+    var templateThree = (
+        <div>
+            <h1>{userName}</h1>
+            <p>Age: {userAge}</p>
+            <p>Location: {userLocation}</p>
+        </div>
+    );
+
+    var appRoot = document.getElementById('app')
+    ReactDOM.render(templateThree, appRoot)
+
+
+    //Challenge: 6. JSX Expressions
+        //create app object: string/subtitle
+        // use title/subtitle
+        //render template
+    
+    var userInfo = {
+        title: 'Suborna J',
+        subtitle: 'This is a short info about Suborna'
+    }
+
+    var templateFour = (
+        <div>
+            <h2>Title: {userInfo.title}</h2>
+            <h3>Subtitle: {userInfo.subtitle}</h3>
+        </div>
+    );
+    
+    var appRoot = document.getElementById('app')
+    ReactDOM.render(templateFour, appRoot)
         
+    //Tutorial: 7. Conditional Rendering in JSX
+    
+    var userInfo = {
+        title: 'Suborna Jahan',
+        subtitle: 'This is a short info about Suborna',
+        job: 'UI/UX designer',
+        age: 27
+    }
+
+    function getSubtitle(subtitle) {
+        if (subtitle) {
+            return <h3>Subtitle: {subtitle}</h3>
+        } 
+    }
+
+    var templateFive = (
+        <div>
+            <h1>{userInfo.title ? userInfo.title : 'Anonymous'}</h1>
+            {getSubtitle(userInfo.subtitle)}
+            <h4>Job: {userInfo.job}</h4>
+            {(userInfo.age && userInfo.age >= 18) && <h5>Age: {userInfo.age}</h5>}
+        </div>
+    );
+    
+    var appRoot = document.getElementById('app')
+    ReactDOM.render(templateFive, appRoot)
+    
+    //Challenge: 7. Conditional Rendering in JSX
 
 
